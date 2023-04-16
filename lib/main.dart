b994ab93a14c93d496c2f499e5605d8d7bd3bcbe
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2023-04-16 23:33:12 +0900

    seventh commit

--- lib/main.dart
+++ lib/main.dart
@@ -48,14 +48,8 @@ void initializeBannerAd() {
 
 // プラットフォーム（iOS / Android）に合わせてデモ用広告IDを返す
 String getTestAdBannerUnitId() {
-  String testBannerUnitId = "";
-  if (Platform.isAndroid) {
-    // Android のとき
-    testBannerUnitId = "ca-app-pub-3940256099942544/6300978111"; // Androidのデモ用バナー広告ID
-  } else if (Platform.isIOS) {
-    // iOSのとき
-    testBannerUnitId = "ca-app-pub-2209028789060457/4518094197"; // iOSのデモ用バナー広告ID
-  }
+  String testBannerUnitId = "ca-app-pub-2209028789060457/4518094197"; // iOSのデモ用バナー広告ID
+  
   return testBannerUnitId;
 }
 
